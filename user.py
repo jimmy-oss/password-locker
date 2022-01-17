fb3a1a0fcc06a607c655b1da698f92dd368c9e77
jimmy-oss <jimmynjonge51@gmail.com>
2022-01-17 16:31:53 +0300

    user tests

---
+++ user.py
@@ -0,0 +1,17 @@
+class User:
+    """
+    Class that generates new instances of users.
+    """
+
+    user_list = []  # Empty user list
+
+    def __init__(self, first_name, last_name, number, email):
+
+      # docstring removed for simplicity
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.phone_number = number
+        self.email = email
+# always remember to import e.g from user import User
+# Init method up here
---
+++ user_test.py
@@ -0,0 +1,33 @@
+import unittest  # Importing the unittest module
+from user import User  # Importing the user class
+
+
+class TestUser(unittest.TestCase):
+
+    '''
+    Test class that defines test cases for the contact class behaviours.
+
+    Args:
+        unittest.TestCase: TestCase class that helps in creating test cases
+    '''
+
+    def setUp(self):
+        '''
+        Set up method to run before each test cases.
+        '''
+        self.new_contact = User(
+            "Jimmy", "Jammie", "0712345678", "jimmy@ms.com")  # create contact object
+
+    def test_init(self):
+        '''
+        test_init test case to test if the object is initialized properly
+        '''
+
+        self.assertEqual(self.new_contact.first_name, "Jimmy")
+        self.assertEqual(self.new_contact.last_name, "Jammie")
+        self.assertEqual(self.new_contact.phone_number, "0712345678")
+        self.assertEqual(self.new_contact.email, "jimmy@ms.com")
+
+
+if __name__ == '__main__':
+    unittest.main()
