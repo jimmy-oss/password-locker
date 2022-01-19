e905027470a1b44746c27dd3cec9550530608086
jimmy-oss <jimmynjonge51@gmail.com>
2022-01-19 16:48:11 +0300

    credentials unnitest

---
+++ credentials.py
@@ -0,0 +1,13 @@
+class Credentials():
+    """
+       Class that generates new instances of contacts.
+       """
+
+    credentials_list = []  # Empty contact list
+
+    def __init__(self, first_name, number):
+
+        # docstring removed for simplicity
+
+        self.first_name = first_name
+        self.phone_number = number
--- credentials_test.py
+++ credentials_test.py
@@ -1,13 +1,2 @@
-class Credentials():
-    """
-       Class that generates new instances of contacts.
-       """
-
-    credentials_list = []  # Empty contact list
-
-    def __init__(self, first_name, number):
-
-        # docstring removed for simplicity
-
-        self.first_name = first_name
-        self.phone_number = number
+import unittest  # Importing the unittest module
+from credentials import Credentials  # Importing the credentials class
